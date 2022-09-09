761eb8ac29abcea45c157aca35db1e50693db676
IKRDev <112340060+IKRDev@users.noreply.github.com>
2022-09-09 15:14:26 -0500

    Update a.js

--- a.js
+++ a.js
@@ -4,30 +4,30 @@ class MyExtention {
 
   getInfo() {
     return {
-      id: "MyExtention", // Machine-Readable name of the extention, will be used as the namespace
+      "id": "MyExtention", // Machine-Readable name of the extention, will be used as the namespace
 
       // Extention color overrides
-      color1: '#FF8C1A',
-      color2: '#DB6E00',
+      "color1": '#FF8C1A',
+      "color2": '#DB6E00',
 
       // Human-readable name: OPTIONAL
-      name: formatMessage({
-        id: 'MyExtention',
-        defaultMessage: 'My Extention',
-        description: 'Description'
+      "name": formatMessage({
+        "id": 'MyExtention',
+        "defaultMessage": 'My Extention',
+        "description": 'Description'
       }),
 
       //List of the blocks in order of appearence
-      blocks: [
+      "blocks": [
         {
-          opcode: "myReporter",// Name of block, and function it will use
-          blocktype: "reporter", // type of block
-          text: "report [arg]", // Text of blocks (square brackets are arguments)
+          "opcode": "myReporter",// Name of block, and function it will use
+          "blocktype": "reporter", // type of block
+          "text": "report [arg]", // Text of blocks (square brackets are arguments)
           // Block arguments
-          arguments: {
-            arg: { // name
-              type: "string", // type of argument
-              defaultvalue: "string arg" // default value of argument
+          "arguments": {
+            "arg": { // name
+              "type": "string", // type of argument
+              "defaultvalue": "string arg" // default value of argument
             }
           }
         }
